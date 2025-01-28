d1e5b95c2e75f89cffc2c31c311cecf6e72bec49
EmirhanKir <157562654+EmirhanKir@users.noreply.github.com>
2025-01-28 15:13:01 -0500

    Add files via upload

---
+++ MPLLab1/MovieQuoteInfo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MPLLab1;
+
+/**
+ * File: MovieQuoteInfo.java
+ * @author Emirhan Kir
+ */
+
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        // Display the movie quote
+        System.out.println("\"It ain't about how hard you hit. It's about how hard you can get hit and keep moving forward.\"");
+        System.out.println("Movie: Rocky Balboa");
+        System.out.println("Character: Rocky Balboa");
+        System.out.println("Year: 2006");
+    }
+}
---
+++ MPLLab1/SongLyrics.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package MPLLab1;
+
+/**
+ * File: SongLyrics.java
+ * @author Emirhan Kir
+ */
+public class SongLyrics {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("I thought that I was dreamin'");
+        System.out.println("When you said you loved me");
+        System.out.println("The start of nothing");
+        System.out.println("I had no chance to prepare");
+    }
+    
+}
